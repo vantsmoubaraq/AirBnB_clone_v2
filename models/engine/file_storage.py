d548a1775c8c25a7dc280f21d6db92e5cd86349e
lukwagoraymond <lukwagoraymond3@gmail.com>
2023-01-06 17:47:44 +0300

    Corrected def all() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,7 +15,7 @@ class FileStorage:
             for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
                     new_dict[key] = value
-                return new_dict
+            return new_dict
         return self.__objects
 
     def new(self, obj):
