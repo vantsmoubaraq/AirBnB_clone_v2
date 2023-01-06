04f525e4fb8ff1d9a82a622e0a311158f81ef1d2
lukwagoraymond <lukwagoraymond3@gmail.com>
2023-01-06 20:50:25 +0300

    Pycodestyle Correction

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,7 @@ class DBStorage:
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session
