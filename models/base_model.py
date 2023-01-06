d6521f06a8524411c2e6c80857f92117859e40f8
lukwagoraymond <lukwagoraymond3@gmail.com>
2023-01-06 17:08:34 +0300

    Task 1 to Tasks 2

---
+++ models/base_model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
+import models
+import uuid
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+time = '%Y-%m-%dT%H:%M:%S.%f'
+if models.storage_type == 'db':
+    Base = declarative_base()
+else:
+    Base = object
+
+
+class BaseModel:
+    """A base class for all hbnb models"""
+    if models.storage_type == 'db':
+        id = Column(String(60), nullable=False, primary_key=True)
+        created_at = Column(DateTime(), nullable=False, default=datetime.now)
+        updated_at = Column(DateTime(), nullable=False, default=datetime.now)
+
+    def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs['updated_at'], time)
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time)
+            else:
+                self.updated_at = datetime.now()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__': self.__class__.__name__})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            del(dictionary["_sa_instance_state"])
+        return dictionary
+
+    def delete(self):
+        """Delete current instance from storage <models.storage> by calling this"""
+        models.storage.delete(self)
---
+++ models/place.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+import models
+import sqlalchemy
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
+if models.storage_type == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('place.id', onupdate='CASCADE',
+                                                                    ondelete='CASCADE'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id', onupdate='CASCADE',
+                                                                      ondelete='CASCADE'),
+                                 primary_key=True, nullable=False)
+                          )
+
+
+class Place(BaseModel, Base):
+    """ A place to stay """
+    if models.storage_type == 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer(), nullable=False, default=0)
+        number_bathrooms = Column(Integer(), nullable=False, default=0)
+        max_guest = Column(Integer(), nullable=False, default=0)
+        price_by_night = Column(Integer(), nullable=False, default=0)
+        latitude = Column(Float(), nullable=True)
+        longitude = Column(Float(), nullable=True)
+        reviews = relationship("Review", backref="place", cascade="all, delete")
+        amenities = relationship("Amenity", secondary='place_amenity',
+                                 backref="place_amenities", viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+    def __int__(self, *args, **kwargs):
+        """initialises Place"""
+        super().__int__(*args, **kwargs)
+
+    if models.storage_type != 'db':
+        @property
+        def reviews(self):
+            from models.review import Review
+            """File Storage relationship between Reviews & Place"""
+            review_list = []
+            all_reviews = models.storage.all(Review)
+            for city in all_reviews.values():
+                if Review.place_id == self.id:
+                    review_list.append(city)
+            return review_list
+
+        @property
+        def amenities(self):
+            from models.place import Amenity
+            """File Storage relationship between Amenity & Place"""
+            amenity_list = []
+            all_amenities = models.storage.all(Amenity)
+            for amenity in all_amenities.values():
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
