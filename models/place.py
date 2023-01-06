3a078bfaa100686987f35760b016d6609caa5149
lukwagoraymond <lukwagoraymond3@gmail.com>
2023-01-06 20:28:15 +0300

    Foreign Key Error

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 if models.storage_type == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
-                                 ForeignKey('place.id',
+                                 ForeignKey('places.id',
                                             onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True, nullable=False),
