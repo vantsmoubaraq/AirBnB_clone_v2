2768d831ac19050e00a3efa33ebe2f5cbfa5813a
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-04 09:31:25 +0300

    web_flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def cisfun(text):
 
 
 @app.route("/python", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def pythoniscool(text="is cool"):
     """
     display “Python ”, followed by the value of the text variable
