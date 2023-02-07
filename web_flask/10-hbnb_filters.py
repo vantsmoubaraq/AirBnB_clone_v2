7720aecc7127d9cc6f7e951fe5c44cbf1a975dd2
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-07 04:27:06 +0300

    web_flask

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-
-"""Display HTML page 6-index.html"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-
 app = Flask(__name__)
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
+@app.route('/hbnb_filters', strict_slashes=False)
 def filters():
-    """Display,states, cities and amenities"""
-    states = list(storage.all(State).values())
-    amenities = list(storage.all(Amenity).values())
-    return render_template("10-hbnb_filters.html", states=states,
+    """display a HTML page like 6-index.html from static"""
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=states,
                            amenities=amenities)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Ends Session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-
-"""Display all states or one state if it has id"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def all_states():
-    """Display all states"""
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def one_state(id):
-    """Display one state"""
-    states = list(storage.all(State).values())
-    return render_template("9-states.html", states=states, id=id)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """Ends Session"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
