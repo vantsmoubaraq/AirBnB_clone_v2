ab58a09c7ea6416f7ca227bfb7bba99ea698eaff
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-07 04:35:44 +0300

    web_flask

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,40 +1,55 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+""" Script starts a Flask Web Application
+listening on 0.0.0.0, port 5000
 """
-from models import storage
-from flask import Flask
-from flask import render_template
+
+from flask import Flask, render_template
+import models
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """Displays HTML of all States in Stored Chronologically"""
+    states = models.storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+@app.route('/cities_by_states', strict_slashes=False)
+def list_state_cities():
+    """Displays HTML of states and its cities"""
+    states = models.storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+def list_states():
+    """Displays HTML of states Chronologically"""
+    states = models.storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    """Displays HTML of States based on id"""
+    flag = 0
+    states = None
+    all_states = models.storage.all(State).values()
+    for state in all_states:
+        if id in state.id:
+            flag = 1
+            states = state
+            break
+    return render_template('9-states.html', states=states, flag=flag)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
+def teardown(db):
+    """Closes or otherwise deallocates resource if it exists"""
+    models.storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
